e57f7eef9b6e956717739e5e807e23dae6a56c4e
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-30 15:48:34 -0500

    customtkinter messing around

--- Phase.py
+++ Phase.py
@@ -4,8 +4,8 @@ import customtkinter
 customtkinter.set_appearance_mode("System")  # Modes: system (default), light, dark
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("700x800")
+root = customtkinter.CTk()  # create CTk window like you do with the Tk window
+root.geometry("700x800")
 
 # def button_function():
 #     print("button pressed")
@@ -15,7 +15,7 @@ app.geometry("700x800")
 # button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
 # button.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
-menu = customtkinter.CTkButton(master=app, text="MENU", fg_color="red")
+menu = customtkinter.CTkButton(master=root, text="MENU", fg_color="red")
 menu.place(relx=0.1, rely=0.02, anchor=tkinter.CENTER)
 
 # cont = customtkinter.CTkButton(master=app, text="Continue")
@@ -24,4 +24,4 @@ menu.place(relx=0.1, rely=0.02, anchor=tkinter.CENTER)
 # exit = customtkinter.CTkButton(master=app, text="Exit Simulation")
 # exit.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
-app.mainloop()
+root.mainloop()
--- Start.py
+++ Start.py
@@ -6,17 +6,17 @@ customtkinter.set_appearance_mode("System")  # Modes: system (default), light, d
 customtkinter.set_default_color_theme("dark-blue")  # Themes: blue (default), dark-blue, green
 
 
-app = customtkinter.CTk()  # create CTk window like you do with the Tk window
-app.geometry("700x800")
+root = customtkinter.CTk()  # create CTk window like you do with the Tk window
+root.geometry("700x800")
 
 
-new_sim = customtkinter.CTkButton(master=app, text="New Simulation", fg_color="violet", text_color="Black")
+new_sim = customtkinter.CTkButton(master=root, text="New Simulation", fg_color="violet", text_color="Black")
 new_sim.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-cont = customtkinter.CTkButton(master=app, text="Continue", fg_color="Green")
+cont = customtkinter.CTkButton(master=root, text="Continue", fg_color="Green")
 cont.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
 
-leave = customtkinter.CTkButton(master=app, text="Exit Simulation", fg_color="red")
+leave = customtkinter.CTkButton(master=root, text="Exit Simulation", fg_color="red")
 leave.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
-app.mainloop()
+root.mainloop()
