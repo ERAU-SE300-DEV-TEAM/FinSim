baa97f977689d97cccd4f15291d54bb8c5ba72c9
Taylor Jensen <Taylorlcj@gmail.com>
2023-01-29 22:57:56 -0500

    customtkinter

--- main.py
+++ main.py
@@ -7,8 +7,10 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 app = customtkinter.CTk()  # create CTk window like you do with the Tk window
 app.geometry("400x240")
 
+
 def button_function():
     print("button pressed")
+
 
 # Use CTkButton instead of tkinter Button
 button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
