d967b37e5f4e95419a95cde1ad67d49d1ee6e7b9
Taylor Jensen <Taylorlcj@gmail.com>
2023-02-01 15:55:29 -0500

    background image runs!!!!

--- BackgroundImage.py
+++ BackgroundImage.py
@@ -30,23 +30,5 @@ cont.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
 leave = customtkinter.CTkButton(master=root, text="Exit Simulation", fg_color="red")
 leave.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
-'''
-# Create a label
-my_label = Label(root, image=bg)
-my_label.place(x=0, y=0, relwidth=1, relheight=1)
-# Add something to the top of our image
-my_text = Label(root, text="Welcome!", font=("Helvetica", 50), fg="white", bg="#2a1863")
-my_text.pack(pady=50)
-# create a frame
-my_frame = Frame(root, bg='#6b88fe')
-my_frame.pack(pady=20)
-# Add some buttons
-my_button1 = Button(my_frame, text="Exit")
-my_button1.grid(row=0, column=0, padx=20)
-my_button2 = Button(my_frame, text="Start")
-my_button2.grid(row=0, column=1, padx=20)
-my_button3 = Button(my_frame, text="Reset")
-my_button3.grid(row=0, column=2, padx=20)
-'''
 
 root.mainloop()
