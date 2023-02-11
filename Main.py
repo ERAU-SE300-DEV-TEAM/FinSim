2ac4a11c443ac398ad5945b5108c1369e04007ad
Chris (Kino) <bierleychris@gmail.com>
2023-02-11 17:41:07 -0500

    updating

--- Main.py
+++ Main.py
@@ -6,17 +6,20 @@ customtkinter.set_appearance_mode("System")  # Modes: system (default), light, d
 customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-blue, green
 
 
-root = customtkinter.CTk()  # create CTk window like you do with the Tk window
+root = tkinter.CTk()  # create CTk window like you do with the Tk window
 root.geometry("700x800")
 
+#button = tk.Button(self, text="Visit Page 1",
+#                           command=lambda: controller.show_frame(PageOne))
 
-new_sim = customtkinter.CTkButton(master=root, text="New Simulation", fg_color="violet", text_color="Black")
+
+new_sim = tkinter.Button(master=root, text="New Simulation", fg_color="violet", text_color="Black")
 new_sim.place(relx=0.5, rely=0.3, anchor=tkinter.CENTER)
 
-cont = customtkinter.CTkButton(master=root, text="Continue", fg_color="Green")
+cont = tkinter.Button(master=root, text="Continue", fg_color="Green")
 cont.place(relx=0.5, rely=0.45, anchor=tkinter.CENTER)
 
-leave = customtkinter.CTkButton(master=root, text="Exit Simulation", fg_color="red")
+leave = tkinter.Button(master=root, text="Exit Simulation", fg_color="red")
 leave.place(relx=0.5, rely=0.6, anchor=tkinter.CENTER)
 
 root.mainloop()
