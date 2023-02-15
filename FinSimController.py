f39706c04062a79aad56c9e6f0301d8e3df90dd9
TWykle <thomaswykle2002@gmail.com>
2023-02-15 16:29:42 -0500

    Move Calc Money Functions to Controller

--- FinSimController.py
+++ FinSimController.py
@@ -7,13 +7,23 @@ class Player:
     income : 0.0
     currentMoney: 0.0
     monthlyLosses: 0.0
+    currentMonth: 1
 
 
 # Class/ Attribute Instantiations/ Declarations
-player = Player( 0, 0, 0,)
+player = Player( 0, 0, 0, 1)
 
 
 # Function Defintions
+
+def menu():
+    startingMoneyChoice = int((input("Please select a starting money amount\n1: 1,000 \n2: 2,000 \n3: 3,000\n")))
+    if startingMoneyChoice == 1:
+        player.currentMoney = 1000
+    if startingMoneyChoice == 2:
+        player.currentMoney = 2000
+    if startingMoneyChoice == 3:
+        player.currentMoney = 3000
 
 # In game this calcMoneyImmediate function is to adjust money on an ad hoc basis. Meant to be run for immediate payments or adjustments that are not monthly
 # valueChange should be positive for increase and negative for decreases
--- FinSimMain.py
+++ FinSimMain.py
@@ -3,17 +3,10 @@ import array as arr
 from FinSimController import *
 
 
-# Function Definitions
-def menu():
-    startingMoneyChoice = int((input("Please select a starting money amount\n1: 1,000 \n2: 2,000 \n3: 3,000\n")))
-    if startingMoneyChoice == 1:
-        player.currentMoney = 1000
-    if startingMoneyChoice == 2:
-        player.currentMoney = 2000
-    if startingMoneyChoice == 3:
-        player.currentMoney = 3000
+# Function Definition
 
 
 # Function Calls
 menu()
+getPlayerMoney()
 
