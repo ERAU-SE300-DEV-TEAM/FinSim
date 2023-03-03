593ad3fb77a6863ad245887f5386ac6e0bacfcfb
Taylor Jensen <Taylorlcj@gmail.com>
2023-03-03 15:54:04 -0500

    deleted this nonsense

--- fileStructureAttempt/main.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-import fileStructureAttempt.Menu as Menu
-from fileStructureAttempt.Menu import *
-from fileStructureAttempt.Simulator import *
-
-if __name__ == "__main__":
-    app = Simulator()
-    app.mainloop()
